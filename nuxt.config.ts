383dfae526f823d4e94017fbac2e6bca7b27e961
boris <boris.stoy@gmail.com>
2019-10-16 09:19:20 -0400

    Add font, make tsconfig ignore libs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,13 +22,23 @@ export default {
             content: process.env.npm_package_description || ''
         }
         ],
-        link: [{
-            rel: 'icon',
-            type: 'image/x-icon',
-            href: '/favicon.ico'
-        }]
+        link: [
+            {
+                rel: 'icon',
+                type: 'image/x-icon',
+                href: '/favicon.ico'
+            },
+            {
+                rel: 'stylesheet',
+                href:
+                    'https://fonts.googleapis.com/css?family=Overpass:300,400,700,900&display=swap'
+            }
+        ]
     },
 
+    /**
+     * Register server routes
+     */
     serverMiddleware: [
         { path: '/api', handler: '~/server/api/index.ts' }
     ],
@@ -39,11 +49,18 @@ export default {
     loading: {
         color: '#fff'
     },
+
+    /**
+     * Specify build output directory name
+     */
+    buildDir: 'nuxt-build',
+
     /*
     ** Global CSS
     */
     css: [
-        'element-ui/lib/theme-chalk/index.css'
+        'element-ui/lib/theme-chalk/index.css',
+        '@/assets/app.sass'
     ],
     /*
     ** Plugins to load before mounting the App
--- types/vue.d.ts
+++ types/vue.d.ts
@@ -1,13 +1,7 @@
-import { ElLoadingComponent, LoadingServiceOptions } from 'element-ui/types/loading'
 import Vue, { ComponentOptions } from 'vue'
 import CookieUniversalNuxt from 'cookie-universal-nuxt'
 import VueRouter from 'vue-router'
 import config from '@/libs/config'
-
-/**
- * Missing type fix
- * Reference: https://stackoverflow.com/questions/49090240/nuxt-vue-js-in-typescript-object-literal-may-only-specify-known-properties-b
- */
 
 declare module 'vue/types/vue' {
     // eslint-disable-next-line
@@ -16,17 +10,19 @@ declare module 'vue/types/vue' {
         $config: typeof config
         CookieUniversalNuxt: typeof CookieUniversalNuxt
         window: typeof window
+        // This is a conflict betweeb Element plugin and Nuxt types. No time to fix this.
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        // THis is a conflict betweeb Element plugin and Nuxt types. No time to fix this.
+        // Issue listed here: https://github.com/ElemeFE/element/issues/17630
     }
 }
 
 declare module 'vue/types/options' {
     interface IComponentOptions<V extends Vue> {
         // This adds the `middleware` property to the existing `vue/types/options/ComponentOptions` type
+        // Missing type fix
+        // Reference: https://stackoverflow.com/questions/49090240/nuxt-vue-js-in-typescript-object-literal-may-only-specify-known-properties-b
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         middleware?: string | string[] | any
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        $loading (options: any): any
     }
 }
