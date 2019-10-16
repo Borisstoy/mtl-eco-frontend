69811944c48c9dc0e8c0c7cf1362da1d4b6f7381
boris <boris.stoy@gmail.com>
2019-10-16 14:32:23 -0400

    Try typescript fix - Really YOLO

--- types/vue.d.ts
+++ types/vue.d.ts
@@ -17,7 +17,7 @@ declare module 'vue/types/vue' {
 }
 
 declare module 'vue/types/options' {
-    interface IComponentOptions<V extends Vue> {
+    interface ComponentOptions<V extends Vue> {
         // This adds the `middleware` property to the existing `vue/types/options/ComponentOptions` type
         // Missing type fix
         // Reference: https://stackoverflow.com/questions/49090240/nuxt-vue-js-in-typescript-object-literal-may-only-specify-known-properties-b
