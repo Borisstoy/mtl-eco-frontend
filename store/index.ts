b3a1ddd731b7e03117cc8c28d7086b2e0f540744
boris <boris.stoy@gmail.com>
2019-10-18 15:16:39 -0400

    Add comments

--- store/index.ts
+++ store/index.ts
@@ -3,6 +3,7 @@ export const actions = {
         let token = ''
         if (req.headers.cookie) {
             const parsed = this.$cookies.nodeCookie.parse(req.headers.cookie)
+            // Clean token: replace single by double quotes, stringify it ready to be parsed
             const cleaned = JSON.stringify(parsed.accessToken.replace(/'/g, '"'))
             try {
                 token = JSON.parse(cleaned)
