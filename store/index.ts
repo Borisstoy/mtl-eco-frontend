de788722b74d7e24194d93f87067ba6546970fc8
boris <boris.stoy@gmail.com>
2019-10-18 15:08:06 -0400

    Fix cookie parsing

--- store/index.ts
+++ store/index.ts
@@ -3,8 +3,9 @@ export const actions = {
         let token = ''
         if (req.headers.cookie) {
             const parsed = this.$cookies.nodeCookie.parse(req.headers.cookie)
+            const cleaned = JSON.stringify(parsed.accessToken.replace(/'/g, '"'))
             try {
-                token = JSON.parse(parsed.accessToken)
+                token = JSON.parse(cleaned)
                 commit('user/setAuth', token)
             }
             catch (err) {
